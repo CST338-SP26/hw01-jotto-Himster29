4a7ec63cf4e4dc5940c69dd41396e95c83ec4f30
Himster29 <hyapa@csumb.edu>
2026-02-07 15:20:40 -0800

    lap top transfer

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -1,7 +1,76 @@
 /**
- * @author feng3302
- * @version 0.1.0
- * @Since 1/29/26
+ * name: Himansu Yapa
+ * Class: CST 348
+ * Project: Jotto first assignment
+ *
  **/
+
+import java.util.ArrayList;
+
 public class Jotto {
+    //Variables
+    int WORD_SIZE, score;
+    String currentWord = "";
+    String filename = "";
+    ArrayList<String> playGuesses = new ArrayList<>();
+    ArrayList<String> playWords = new ArrayList<>();
+    ArrayList<String> wordList = new ArrayList<>();
+    boolean DEBUG;
+
+    //Methods
+    public boolean pickWord(){
+        return false;
+    }
+
+    public String showWordList(){
+        return "";
+    }
+
+    public ArrayList<String> showPlayerGuesses() {
+
+    }
+
+    void playerGuessScores(ArrayList<String> word) {
+
+    }
+
+    public void setCurrentWord(String word){
+
+    }
+
+    public ArrayList<String> readWords(String word){
+
+    }
+
+    public void play(){
+
+    }
+
+    int guess(){
+
+    }
+
+    public ArrayList<String> getPlayedWords() {
+
+    }
+
+    public String getCurrentWord(){
+
+    }
+
+    public int getLetterCount(String word){
+
+    }
+
+    public String showPlayedWords(){
+
+    }
+
+    public boolean addPlayerGuess(String word){
+
+    }
+
+    void updateWordList(){
+
+    }
 }
